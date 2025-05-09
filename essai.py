a34d11ed56f52cf79edc88e01019b7b4d416be2b
Simeon Wansi <w.simeon06@yahoo.com>
2025-05-09 22:39:45 +0400

    step1: Chat boot + 2 tools: Fuel cost and Weather

--- essai.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-st.title("My first app")
-st.write("Here's our first attempt at using data to create a table")
-name = st.text_input("Enter your name")
-if name:
-    st.write("Hello", name, "Nice to meet you")
